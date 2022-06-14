c204c936d33f67c20a154b6d2a4fb076650c4064
NandoApri <apriando.pratamaa@gmail.com>
2022-06-14 23:17:43 +0700

    Template-Admin

---
+++ resources/views/Template-Admin/footer.blade.php
@@ -0,0 +1,34 @@
+<!-- footer -->
+<div class="container-fluid">
+    <div class="footer">
+        <p>Copyright © 2018 Designed by html.design. All rights reserved.<br><br>
+            Distributed By: <a href="https://themewagon.com/">ThemeWagon</a>
+        </p>
+    </div>
+</div>
+</div>
+<!-- end dashboard inner -->
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- wow animation -->
+<script src="js/animate.js"></script>
+<!-- select country -->
+<script src="js/bootstrap-select.js"></script>
+<!-- owl carousel -->
+<script src="js/owl.carousel.js"></script>
+<!-- chart js -->
+<script src="js/Chart.min.js"></script>
+<script src="js/Chart.bundle.min.js"></script>
+<script src="js/utils.js"></script>
+<script src="js/analyser.js"></script>
+<!-- nice scrollbar -->
+<script src="js/perfect-scrollbar.min.js"></script>
+<script>
+    var ps = new PerfectScrollbar('#sidebar');
+</script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+<script src="js/chart_custom_style1.js"></script>
